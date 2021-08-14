1bfb83955b3c56b09cf2b875de2d93e6fbd7f40d
Merve San <sanmerve@hotmail.com.tr>
2021-08-13 23:43:48 -0400

    merve messes up

---
+++ src/test/java/com/googleproject/tinytest/Navigations.java
@@ -0,0 +1,9 @@
+package com.googleproject.tinytest;
+
+import java.util.WeakHashMap;
+
+public class Navigations {
+    public static void main(String[] args) {
+
+    }
+}
--- src/test/java/hi.java
+++
@@ -1,6 +0,0 @@
-public class hi {
-    public static void main(String[] args) {
-        //hi
-        //hi 2
-    }
-}
