fa46a693000432882e580788d84994fd171e2d1e
Merve San <sanmerve@hotmail.com.tr>
2021-08-13 23:49:26 -0400

    merve added webdrivermanager

--- src/test/java/com/googleproject/tinytest/Navigations.java
+++ src/test/java/com/googleproject/tinytest/Navigations.java
@@ -1,9 +1,11 @@
 package com.googleproject.tinytest;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import java.util.WeakHashMap;
 
 public class Navigations {
     public static void main(String[] args) {
-
+        WebDriverManager.chromedriver().setup();
     }
 }
